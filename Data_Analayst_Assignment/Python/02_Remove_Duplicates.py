8eb526612100dd56d01d3ba0502109e8171f5999
Chakri-commits <gchakri2003@gmail.com>
2025-12-03 10:11:40 +0530

    Implement user input for remove_duplicates function
    Added a main block to allow user input for duplicate removal.

---
+++ Data_Analayst_Assignment/Python/02_Remove_Duplicates.py
@@ -0,0 +1,10 @@
+def remove_duplicates(s):
+    unique = ""
+    for ch in s:
+        if ch not in unique:
+            unique += ch
+    return unique
+
+if __name__=="__main__":
+  s=str(input())
+print(remove_duplicates(s))
