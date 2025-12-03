903aaa1de42726dd158e95a3480557d26ab5a510
Chakri-commits <gchakri2003@gmail.com>
2025-12-03 10:10:10 +0530

    Create 01_Time_Converter.py

---
+++ Data_Analayst_Assignment/Python/01_Time_Converter.py
@@ -0,0 +1,11 @@
+def convert_minutes(m):
+    hours = m // 60
+    minutes = m % 60
+    if hours == 1:
+        return f"{hours} hr {minutes} minutes"
+    return f"{hours} hrs {minutes} minutes"
+if __name__=="__main__":
+  m=int(input())
+  m1=int(input())
+print(convert_minutes(m))  
+print(convert_minutes(m1))  
